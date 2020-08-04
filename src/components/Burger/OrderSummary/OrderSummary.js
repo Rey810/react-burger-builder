06a3c507dc58693a390ed87e0f35a13136b3cf09
reyvdb <Rey810@users.noreply.github.com>
2020-08-04 17:19:21 +0200

    optimize Modal component performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
-    );
-  });
+class OrderSummary extends Component {
+  // includes a lifecycle hook as changes to the data in the modal should only trigger a re-render of the modal and the nested OrderSummary if the modal is visible
+  // this lifecycle hook is for debugging purposes. This component will update every time there is a change in the Modal
+  componentDidUpdate() {
+    // this (and the re-rerendering of this component) is controlled by what happens in the lifecycle hook in the Modal
+    console.log("[OrderSummary] did update");
+  }
 
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>Continue to Checkout</p>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </>
-  );
-};
-export default orderSummary;
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>Continue to Checkout</p>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
+}
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-  <>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+class Modal extends Component {
+  // We only want the modal to update if "show" changes to prevent unnecessary re-rendering every time an order detail changes and the modal isn't showing [perf]
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentDidUpdate() {
+    console.log("[Modal] did update");
+  }
+
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Modal;
