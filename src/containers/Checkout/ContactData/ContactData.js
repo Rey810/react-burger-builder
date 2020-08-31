bf7fd3dccb086266bb295c642a3a18ebc7a0b11e
Rey van den Berg <reyvdb@gmail.com>
2020-08-31 13:13:25 +0200

    add dynamically created form inputs

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,14 +3,60 @@ import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "./ContactData.module.css";
 import axios from "../../../axios-orders";
-
+import Input from "../../../components/UI/Input/Input";
 class ContactData extends Component {
   state = {
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: "",
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your Name",
+        },
+        value: "Rey",
+      },
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Street",
+        },
+        value: "31 Something Str.",
+      },
+      zipCode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "ZIP CODE",
+        },
+        value: "",
+      },
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Country",
+        },
+        value: "",
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your Email",
+        },
+        value: "",
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" },
+          ],
+        },
+        value: "",
+      },
     },
     loading: false,
   };
@@ -24,15 +70,6 @@ class ContactData extends Component {
       ingredients: this.props.ingredients,
       // production version would not have this here but rather re-calculated on the server
       price: this.props.price,
-      customer: {
-        name: "Rey",
-        address: {
-          street: "Test Street",
-          zipCode: "1223",
-          country: "South Africa",
-        },
-        email: "some@email.com",
-      },
       deliveryMethod: "priority",
     };
     // targets firebase endpoint (mongoDB-like structure)
@@ -46,32 +83,23 @@ class ContactData extends Component {
   };
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        setup: this.state.orderForm[key],
+      });
+    }
     let form = (
-      <form action="" className={classes.ContactData}>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your Name"
-        />
-        <input
-          className={classes.Input}
-          type="email"
-          name="email"
-          placeholder="Your Email"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Your Street"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="postal"
-          placeholder="Your Postal Code"
-        />
+      <form action="">
+        {formElementsArray.map((formElement) => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.setup.elementType}
+            elementConfig={formElement.setup.elementConfig}
+            value={formElement.setup.value}
+          />
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>
           ORDER
         </Button>
@@ -81,7 +109,7 @@ class ContactData extends Component {
       form = <Spinner />;
     }
     return (
-      <div>
+      <div className={classes.ContactData}>
         <h4>Enter Your Contact Data</h4>
         {form}
       </div>
